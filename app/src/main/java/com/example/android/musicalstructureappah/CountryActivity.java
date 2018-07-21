98c268767cf17f2d5a3576acda6aeb473a484215
lehieu30794 <lehieu30794@gmail.com>
2018-07-21 15:20:12 -0600

    Use convertView variable instead of declaring a new one (MusicAdapter)

--- app/src/main/java/com/example/android/musicalstructureappah/CountryActivity.java
+++ app/src/main/java/com/example/android/musicalstructureappah/CountryActivity.java
@@ -2,6 +2,10 @@ package com.example.android.musicalstructureappah;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class CountryActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class CountryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_country);
+
+        ArrayList<String> musicInfo = new ArrayList<String>();
+
+        musicInfo.add("Tequila");
+
+        ArrayAdapter<String> musicItem = new ArrayAdapter<String>(this,
+                R.layout.list_view, musicInfo);
+
+        ListView listView = (ListView) findViewById(R.id.activity_country);
     }
 }
--- app/src/main/java/com/example/android/musicalstructureappah/MusicAdapter.java
+++ app/src/main/java/com/example/android/musicalstructureappah/MusicAdapter.java
@@ -24,16 +24,15 @@ public class MusicAdapter extends ArrayAdapter<Music> {
     @Override
     public View getView(int position, View convertView,
                         ViewGroup parent) {
-        View listItemView = convertView;
-//        Why just use convertView does not work and had to declare a variable View to represent convertView?
-        if (listItemView == null) {
+
+        if (convertView == null) {
             /*No need to find ListView here, just inflate a new list view
             ListView l = (ListView) convertView.findViewById(R.id.list_view);*/
 
 //            LayoutInflater is an interesting object. It does not need to be declared with a 'new' keyword
 //            After that, it calls a method named 'from', then 'inflate' with different input paras
 //            getContext() here probably means MusicAdpater object will get context of the current activity which calls MusicAdapter
-            listItemView = LayoutInflater.from(getContext()).inflate(
+            convertView = LayoutInflater.from(getContext()).inflate(
                     R.layout.list_view, parent, false);
         }
 
@@ -41,14 +40,14 @@ public class MusicAdapter extends ArrayAdapter<Music> {
         Music currentItem = getItem(position);
 
 //        Have to use 'convertView.' to call findViewById. Why? knowing that I am modifying getView method
-        TextView songTextView = (TextView) listItemView.findViewById(R.id.song_text_view);
+        TextView songTextView = (TextView) convertView.findViewById(R.id.song_text_view);
 //        Call a method getSong declared in Music class
         songTextView.setText(currentItem.getSong());
 
-        TextView artistTextView = (TextView) listItemView.findViewById(R.id.artist_text_view);
+        TextView artistTextView = (TextView) convertView.findViewById(R.id.artist_text_view);
         artistTextView.setText(currentItem.getArtist());
 
 
-        return listItemView;
+        return convertView;
     }
 }
