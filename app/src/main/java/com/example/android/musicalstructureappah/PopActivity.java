e69f332dada270ff01fb77a2319155ab044ef373
lehieu30794 <lehieu30794@gmail.com>
2018-07-21 14:04:13 -0600

    Change pop_view into list_view for universal use later on

--- app/src/main/java/com/example/android/musicalstructureappah/PopActivity.java
+++ app/src/main/java/com/example/android/musicalstructureappah/PopActivity.java
@@ -8,6 +8,6 @@ public class PopActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pop);
+        setContentView(R.layout.list_view);
     }
 }
